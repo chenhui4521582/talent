c74bc0b03909ce5bea0b1afee7ed339b5108da3e
panzhanfei <13679383435@163.com>
2020-08-17 11:20:30 +0800

    准备创建2.2 版本分支

--- .umirc.ts
+++ .umirc.ts
@@ -245,10 +245,6 @@ export default defineConfig({
           component: '@/pages/WFmanage/ApplicationRecord',
         },
         {
-          path: '/talent/wfmanage/newform/:id',
-          component: '@/pages/WFmanage/NewForm',
-        },
-        {
           path: '/talent/wfmanage/editRule/:id',
           component: '@/pages/WFmanage/EditRule',
         },
